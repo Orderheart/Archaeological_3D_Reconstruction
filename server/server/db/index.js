0dcb59639474fc07f8a8be3c115a57776511d006
way <133030423+wayc04@users.noreply.github.com>
2024-06-24 21:04:14 +0800

    Update index.js

--- server/server/db/index.js
+++ server/server/db/index.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 // 创建数据库连接对象
 const db = mysql.createPool({
-    host: '8.137.125.44',
-    user:'root',
-    password:'Zw110404.',
-    database:'db_show'
+    host: '',
+    user:'',
+    password:'',
+    database:''
 })
 
 module.exports = db
